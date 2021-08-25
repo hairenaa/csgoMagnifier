b8529a0373ed6d39abe0693f79b22cfa69bab63b
hairenaa <69110014+hairenaa@users.noreply.github.com>
2021-08-25 20:19:18 +0800

    Add files via upload

---
+++ csgoMagnifier/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace csgoMagnifier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(337, 105);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(195, 190);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.DimGray;
+            this.pictureBox2.Image = global::csgoMagnifier.Properties.Resources.mask1;
+            this.pictureBox2.Location = new System.Drawing.Point(289, 76);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(269, 200);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Red;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        public System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.Timer timer1;
+        public System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ csgoMagnifier/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WIndowLong_Dll;
+
+namespace csgoMagnifier
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public CutScreener cser = null;
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WIndowLong_Dll.WindowLonger.SetWindowLong_Transparent(this.Handle);
+            int x = (int)(0.5 * (this.Width - this.pictureBox1.Width));
+            int y = (int)(0.5 * (this.Height - this.pictureBox1.Height));
+            this.pictureBox1.Location = new System.Drawing.Point(x, y);
+            int x1 = (int)(0.5 * (this.Width - this.pictureBox2.Width));
+            int y1 = (int)(0.5 * (this.Height - this.pictureBox2.Height));
+            this.pictureBox2.Location = new Point(x1, y1);
+            
+            //this.Opacity = 0.5;
+            
+            
+        }
+
+        public void CustomShow(int cutWidth,int cutHeight,int update,float magRate,string shape,string mask) {
+            pictureBox1.Visible = true;
+            pictureBox1.Width =(int)(cutWidth * magRate);
+            pictureBox1.Height = (int)(cutHeight * magRate);
+            pictureBox2.Width = (int)(pictureBox1.Width *1.2);
+            pictureBox2.Height =(int)( pictureBox1.Height*1.2);
+            timer1.Interval = 1000 / update;
+            timer1.Enabled = true;
+            if (shape == StartForm.ShapeType.circular.ToString())
+            {
+                System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
+                gp.AddEllipse(0, 0, pictureBox1.Width - 3, pictureBox1.Height - 3);
+                Region rg = new Region(gp);
+                pictureBox1.Region = rg;
+
+            }
+            else if (shape == StartForm.ShapeType.triangle.ToString())
+            {
+                System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
+
+                Point po1 = new Point(this.pictureBox1.Width/2,0);
+                Point po2 = new Point(0, this.pictureBox1.Height);
+                Point po3 = new Point(this.pictureBox1.Width,this.pictureBox1.Height);
+                //po1 = this.PointToClient(po1);
+                //po2 = this.PointToClient(po2);
+                //po3 = this.PointToClient(po3);
+                gp.AddLine(po1,po2);
+                gp.AddLine(po2, po3);
+                Region rg = new Region(gp);
+                pictureBox1.Region = rg;
+
+
+            }
+
+
+            if (mask == StartForm.MaskType.mask1.ToString())
+            {
+                pictureBox2.Image = Properties.Resources.mask1;
+                //this.Opacity = 0.6;
+                this.pictureBox2.Visible = true;
+            }
+            else if (mask == StartForm.MaskType.mask3.ToString())
+            {
+
+                System.Drawing.Drawing2D.GraphicsPath gp1 = new System.Drawing.Drawing2D.GraphicsPath();
+                gp1.AddEllipse(0, 0, pictureBox2.Width - 3, pictureBox2.Height - 3);
+                Region rg1 = new Region(gp1);
+                pictureBox2.Region = rg1;
+                pictureBox2.Image = Properties.Resources.mask3;
+                //this.Opacity = 0.6;
+                this.pictureBox2.Visible = true;
+
+            }
+            else if (mask == StartForm.MaskType.mask2.ToString())
+            {
+                System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
+
+                Point po1 = new Point(this.pictureBox2.Width / 2, 0);
+                Point po2 = new Point(0, this.pictureBox2.Height);
+                Point po3 = new Point(this.pictureBox2.Width, this.pictureBox2.Height);
+                //po1 = this.PointToClient(po1);
+                //po2 = this.PointToClient(po2);
+                //po3 = this.PointToClient(po3);
+                gp.AddLine(po1, po2);
+                gp.AddLine(po2, po3);
+                Region rg = new Region(gp);
+                pictureBox2.Region = rg;
+                pictureBox2.Image = Properties.Resources.mask2;
+                //this.Opacity = 0.6;
+                this.pictureBox2.Visible = true;
+            }
+            else if (mask == StartForm.MaskType.mask0.ToString())
+            {
+                //this.Opacity = 1;
+                pictureBox2.Visible = false;
+            }
+
+
+            //int z_index = this.Controls.GetChildIndex(pictureBox1);
+            pictureBox1.BringToFront();
+            //this.Controls.SetChildIndex(pictureBox1, z_index);
+            this.Show();
+
+        }
+
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            int x = (int)(0.5 * (this.Width - this.pictureBox1.Width));
+            int y = (int)(0.5 * (this.Height -this.pictureBox1.Height));
+            this.pictureBox1.Location = new System.Drawing.Point(x, y);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try {
+                
+                if (cser == null) {
+                    return;
+                }
+                Image re = cser.Handle();
+                pictureBox1.Image = re;
+            }
+            catch (Exception ex) {
+                string content = ex.ToString();
+                FileUtil.Instance.WriteLog(content);
+            }
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.pictureBox1.Region?.Dispose();
+            this.pictureBox2.Region ?. Dispose();
+            cser.dis?.Dispose();
+            
+        }
+
+      
+    }
+}

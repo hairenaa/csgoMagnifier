31becc5d51330cbd16f6f77595393de0cd243458
hairenaa <69110014+hairenaa@users.noreply.github.com>
2022-01-03 23:57:58 +0800

    Add files via upload

---
+++ csgoMagnifier/FileUtil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace csgoMagnifier
+{
+    class FileUtil:Singlon<FileUtil>
+    {
+
+        public static string logpath = Application.StartupPath+@"\log.txt";
+
+        public void WriteLog(string content)
+        {
+            content = System.DateTime.Now + Environment.NewLine + content + Environment.NewLine;
+            File.AppendAllText(logpath, content);
+        }
+        
+    }
+}
